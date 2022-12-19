9c521d561e7a554d5299f93644de7c33b2a8848a
Joao Viktor <joaoviktor22@hotmail.com>
2022-12-19 12:47:38 -0300

    Udemy Spring Boot Curso
    curso Udemy de Spring Boot

---
+++ 21-hibernate-with-service-demo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,54 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO{
+    private EntityManager entityManager;
+    @Autowired
+    public EmployeeDAOHibernateImpl(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll(){
+        Session currenteSession = entityManager.unwrap(Session.class);
+        Query<Employee> theQuery = currenteSession.createQuery("from Employee",Employee.class);
+        List<Employee> employees = theQuery.getResultList();
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Session currentSession=entityManager.unwrap(Session.class);
+
+        Employee theEmployee = currentSession.get(Employee.class, theId);
+
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        Session currentSession=entityManager.unwrap(Session.class);
+        currentSession.merge(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Session currentSession=entityManager.unwrap(Session.class);
+
+        Query theQuery = currentSession.createQuery("delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId",theId);
+
+        theQuery.executeUpdate();
+
+    }
+}
---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+        List<Employee> theEmployees = employeeService.findAll();
+
+        theModel.addAttribute("employees",theEmployees);
+        return "list-employees";
+    }
+}
